fba951840bb244213105c3a26a24fb3e78de327f
Lewis_Damy <ldamy16@gmail.com>
2025-07-28 03:45:40 -0300

    feat: add website logic rule for changing styles

--- scripts/magicRemover.js
+++ scripts/magicRemover.js
@@ -1,6 +1,18 @@
-import { loadCurrentDomain } from "./helpers/domainHelper.js";
+function getBaseDomainAsync(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const {hostname} = new URL(url);
+      const host = hostname.replace(/^www\./, "");
+      const parts = host.split(".");
+      resolve(parts[0]);
+    } catch (error) {
+      console.error("Invalid URL:", error);
+      reject("Invalid URL: " + error.message);
+    }
+  });
+}
 
-loadCurrentDomain("core").then((domain) => {
+getBaseDomainAsync(window.location.href).then((domain) => {
   // Future: Make HTTP Request to Github -> fetch all domain.json's
   const configUrl = chrome.runtime.getURL(`../websites/${domain}.json`);
 
@@ -12,7 +24,8 @@ loadCurrentDomain("core").then((domain) => {
       }
       return res.json();
     })
-    .then(data => {
+    .then(configRules => {
+      console.log(configRules);
       /* EXECUTION OF JSON RULES */
 
       // 0) Meta Controls
@@ -23,15 +36,24 @@ loadCurrentDomain("core").then((domain) => {
 
       // 2) Style Changes
       // applyStyles(config.style);
+      if (configRules.style) {
+        for (const [selector, styles] of Object.entries(configRules.style || {})) {
+          const el = document.querySelector(selector);
+          if (el && styles) {
+            console.log(`Applying styles to '${selector}':`, styles);
+            Object.assign(el.style, styles);
+          } else {
+            console.warn(`Element not found for selector '${selector}' or no styles defined.`);
+          }
+        }
+      }
 
       // 3) Behavioural Scripts
       // runBehaviourScripts(config.behavior);
 
-      if (data.style) {
-        console.log("Apply style rules");
-      }
     })
     .catch(err => {
+      console.log(`Current URL: ${err}`);
       console.error(`No config found named '${domain}.json'. Reason:`, err.message);
     });
 });
